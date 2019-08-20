b5ab9795de5a9ef7b6b7215816e3cf83dd2dc38a
shuiQiao <17301938067@163.com>
2019-08-20 23:20:48 +0800

    新增zuul跨域配置,添加bootstrap.yml配置文件

---
+++ microservice/microservice-zuul-gateway/src/main/java/com/springcloud/conf/GateWayCorsConfig.java
@@ -0,0 +1,33 @@
+package com.springcloud.conf;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+/**
+ * @author Linshuiqiao
+ * @ClassName: GateWayCorsConfig
+ * @description: 解决网关跨域问题
+ * @date 2019年08月20日 22:45
+ * @version: 1.0
+ **/
+@Component
+@Configuration
+public class GateWayCorsConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        final CorsConfiguration corsConfiguration = new CorsConfiguration();
+//        corsConfiguration.setAllowCredentials(true);
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", corsConfiguration);
+        return new CorsFilter(source);
+    }
+
+}
